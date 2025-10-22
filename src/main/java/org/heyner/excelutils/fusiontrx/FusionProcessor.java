0040fcca309dfe1cd51b2f9a679212d26dccde92
Richard HEYNER <richard.heyner@cgi.com>
2025-10-22 21:11:14 +0200

    Fix TU and Javadoc FusionProcessor

--- src/main/java/org/heyner/excelutils/fusiontrx/FusionProcessor.java
+++ src/main/java/org/heyner/excelutils/fusiontrx/FusionProcessor.java
@@ -15,7 +15,17 @@ import java.io.IOException;
 @Component
 @Slf4j
 public class FusionProcessor {
-
+    /**
+     * Exécute la commande de fusion des fichiers TRX.
+     * <p>
+     * Cette méthode parcourt un répertoire donné, filtre les fichiers Excel contenant "TRX",
+     * et fusionne leur contenu dans un fichier de sortie nommé "FusionTRX.xlsx".
+     *
+     * @param directoryToProcess le dossier à parcourir
+     * @param outputPath le chemin du fichier fusionné
+     * @throws GracefulExitException si aucun fichier TRX n’est trouvé
+     * @throws FatalApplicationException en cas d’erreur technique lors de la fusion
+     */
     public void process(String directoryToProcess, String outputPath) {
 
         File myDirectory = new File(directoryToProcess);
@@ -41,6 +51,20 @@ public class FusionProcessor {
         }
     }
 
+    /**
+     * Fusionne le contenu d’un fichier Excel TRX dans une feuille de fusion.
+     * <p>
+     * Cette méthode ouvre le fichier, vérifie la présence de la feuille "sheet1",
+     * et copie son contenu dans la feuille de fusion en respectant l’offset de ligne.
+     *
+     * @param file le fichier Excel source à fusionner
+     * @param sheetFusion la feuille cible dans le fichier de fusion
+     * @param ignoreFirstLine indique s’il faut ignorer la première ligne (en-têtes)
+     * @param rowOffset l’index de ligne à partir duquel insérer les données
+     * @return le nouvel offset de ligne après insertion
+     * @throws FatalApplicationException en cas d’erreur d’ouverture ou de lecture du fichier
+     * @throws FusionSheetMissingException si la feuille "sheet1" est absente
+     */
     private int mergeFile(File file, Sheet sheetFusion, boolean ignoreFirstLine, int rowOffset) {
         try (ExcelFile excelIn = new ExcelFile(file.getAbsolutePath())) {
             log.info("File {} opened.",file.getName());
@@ -50,6 +74,7 @@ public class FusionProcessor {
             }
             return excelIn.copySheet(sheetIn, sheetFusion, ignoreFirstLine,rowOffset);
         } catch (FusionSheetMissingException e) {
+            log.warn("Skipping file {} due to missing sheet 'sheet1'", file.getName());
             return rowOffset;
         } catch (IOException e) {
             throw new FatalApplicationException(e.getMessage(),-1);
--- src/test/java/org/heyner/excelutils/fusiontrx/FusionProcessorTest.java
+++ src/test/java/org/heyner/excelutils/fusiontrx/FusionProcessorTest.java
@@ -18,7 +18,7 @@ class FusionProcessorTest {
     @BeforeEach
     void setUp() throws IOException {
         TestInitializerFactory.action(this.getClass().getSimpleName());
-        File f = new File ("target/temp-FusionTRXTest/fusion/FusionTRX.xlsx");
+        File f = new File ("target/temp-FusionProcessorTest/fusion/FusionTRX.xlsx");
         if (f.exists())
             f.delete();
     }
@@ -26,10 +26,10 @@ class FusionProcessorTest {
     @Test
     void testFusionTRX() throws IOException {
         FusionProcessor fusionProcessor=new FusionProcessor();
-        fusionProcessor.process( "target/temp-FusionTRXTest/fusion/",
-                                                        "target/temp-FusionTRXTest/fusion/");
+        fusionProcessor.process( "target/temp-FusionProcessorTest/fusion/",
+                                                        "target/temp-FusionProcessorTest/fusion/");
 
-        ExcelFile fichierExcel = new ExcelFile("target/temp-FusionTRXTest/fusion/FusionTRX.xlsx");
+        ExcelFile fichierExcel = new ExcelFile("target/temp-FusionProcessorTest/fusion/FusionTRX.xlsx");
         int rowNum = 0;
         float foreignAmountCum = 0;
         Sheet dataSheet = fichierExcel.getWorkBook().getSheet("Fusion");
