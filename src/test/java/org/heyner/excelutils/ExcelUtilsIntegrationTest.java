36f8c86c26fb0d23373264dd8d5e371adecb5b34
Richard HEYNER <richard.heyner@cgi.com>
2026-05-03 11:22:40 +0200

    test: cleaning CommandTest

--- src/test/java/org/heyner/excelutils/ExcelUtilsIntegrationTest.java
+++ src/test/java/org/heyner/excelutils/ExcelUtilsIntegrationTest.java
@@ -1,5 +1,8 @@
 package org.heyner.excelutils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.heyner.excelutils.application.commands.core.Command;
+import org.heyner.excelutils.application.commands.core.CommandArgs;
 import org.heyner.excelutils.bootstrap.ArgsChecker;
 import org.heyner.excelutils.bootstrap.ExcelUtils;
 import org.junit.jupiter.api.Test;
@@ -13,14 +16,16 @@ import org.springframework.context.annotation.Bean;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
-        classes = ExcelUtils.class,
+        classes = {ExcelUtils.class, ExcelUtilsIntegrationTest.TestConfig.class},
         args = {"servicetest", "arg1", "arg2"}
 )
+@Slf4j
 @ExtendWith(OutputCaptureExtension.class)
 class ExcelUtilsIntegrationTest {
 
     @TestConfiguration
-    static class TestArgsCheckerConfig {
+    static class TestConfig {
+
         @Bean
         public ArgsChecker argsChecker() {
             return new ArgsChecker(null) {
@@ -30,11 +35,33 @@ class ExcelUtilsIntegrationTest {
                 }
             };
         }
+
+        @Bean
+        public Command<CommandArgs> serviceTest() {
+            return new Command<>() {
+                @Override
+                public String name() {
+                    return "servicetest";
+                }
+                @Override
+                public CommandTestArgs parse(String[] args) {
+                    return new CommandTestArgs("servicetest");
+                }
+
+                @Override
+                public void execute(CommandArgs args) {
+                    log.info("Test command executed with args: {}", args);
+                }
+            };
+        }
     }
+
     @Test
     void testMainRunsSuccessfully(CapturedOutput output) {
-        // Vérifie que l'application a bien démarré et exécuté la commande
         assertThat(output.getOut()).contains("Beginning");
         assertThat(output.getOut()).contains("servicetest");
     }
 }
+
+
+record CommandTestArgs(String value) implements CommandArgs {}
